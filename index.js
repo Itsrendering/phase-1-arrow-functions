5f546f20ce1d9bf5c4e7f834f70dd15a942d670d
juanpaolo delacruz <jpmd.creates@gmail.com>
2024-05-23 20:51:37 -0400

    completed

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = (parameter1, parameter2) => parameter1/parameter2
+divide(4,2)
+const square = (parameter1) => parameter1 **2
+square (3)
+const add = (parameter1, parameter2) => parameter1+parameter2
+add (3,4)
